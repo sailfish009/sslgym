8e5ae66ce2c1ed36f61881a4918451db8853066c
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2022-12-24 14:39:21 +0000

    feat(Tensorflow): add custom augmentations with tests

---
+++ src/tensorflow/augmentations.py
@@ -0,0 +1,96 @@
+"""Common Augmentations"""
+import random
+from typing import Any, Callable, Tuple
+
+import tensorflow as tf
+
+
+def shuffle_zipped_output(*args) -> Tuple[Any, ...]:
+    """Shuffle the given inputs"""
+    listify = [*args]
+    random.shuffle(listify)
+    return tuple(listify)
+
+
+@tf.function
+def scale_image(image: tf.Tensor, label: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
+    """Convert all images to float32"""
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    return (image, label)
+
+
+@tf.function
+def solarize(image: tf.Tensor, threshold: int = 128) -> tf.Tensor:
+    """Solarize the input image"""
+    return tf.where(image < threshold, image, 255 - image, name="solarize")
+
+
+@tf.function
+def color_drop(image: tf.Tensor) -> tf.Tensor:
+    """Randomly convert the input image to GrayScale"""
+    image = tf.image.rgb_to_grayscale(image)
+    image = tf.tile(image, [1, 1, 3])
+    return image
+
+
+@tf.function
+def gaussian_blur(
+    image: tf.Tensor, kernel_size: int = 23, padding: str = "SAME"
+) -> tf.Tensor:
+    """
+    Randomly apply Gaussian Blur to the input image
+
+    Reference: https://github.com/google-research/simclr/blob/master/data_util.py
+    """
+
+    sigma = tf.random.uniform((1,)) * 1.9 + 0.1
+    radius = tf.cast(kernel_size / 2, tf.int32)
+    kernel_size = radius * 2 + 1
+    x = tf.cast(tf.range(-radius, radius + 1), tf.float32)
+    blur_filter = tf.exp(
+        -tf.pow(x, 2.0) / (2.0 * tf.pow(tf.cast(sigma, tf.float32), 2.0))
+    )
+    blur_filter /= tf.reduce_sum(blur_filter)
+
+    # One vertical and one horizontal filter.
+    blur_v = tf.reshape(blur_filter, [kernel_size, 1, 1, 1])
+    blur_h = tf.reshape(blur_filter, [1, kernel_size, 1, 1])
+    num_channels = tf.shape(image)[-1]
+    blur_h = tf.tile(blur_h, [1, 1, num_channels, 1])
+    blur_v = tf.tile(blur_v, [1, 1, num_channels, 1])
+    expand_batch_dim = image.shape.ndims == 3
+    if expand_batch_dim:
+        image = tf.expand_dims(image, axis=0)
+    blurred = tf.nn.depthwise_conv2d(
+        image, blur_h, strides=[1, 1, 1, 1], padding=padding
+    )
+    blurred = tf.nn.depthwise_conv2d(
+        blurred, blur_v, strides=[1, 1, 1, 1], padding=padding
+    )
+    if expand_batch_dim:
+        blurred = tf.squeeze(blurred, axis=0)
+    return blurred
+
+
+@tf.function
+def color_jitter(image: tf.Tensor, s: float = 0.5) -> tf.Tensor:
+    """Randomly apply Color Jittering to the input image"""
+    x = tf.image.random_brightness(image, max_delta=0.8 * s)
+    x = tf.image.random_contrast(x, lower=1 - 0.8 * s, upper=1 + 0.8 * s)
+    x = tf.image.random_saturation(x, lower=1 - 0.8 * s, upper=1 + 0.8 * s)
+    x = tf.image.random_hue(x, max_delta=0.2 * s)
+    x = tf.clip_by_value(x, 0, 1)
+    return x
+
+
+@tf.function
+def random_apply(func: Callable, x: tf.Tensor, prob: float) -> tf.Tensor:
+    """Randomly apply the desired func to the input image"""
+    return tf.cond(
+        tf.less(
+            tf.random.uniform([], minval=0, maxval=1, dtype=tf.float32),
+            tf.cast(prob, tf.float32),
+        ),
+        lambda: func(x),
+        lambda: x,
+    )
---
+++ tests/tensorflow/test_augmentations.py
@@ -0,0 +1,106 @@
+"""Test custom tensorflow augmentations."""
+import pytest
+
+import tensorflow as tf
+from src.tensorflow.augmentations import (
+    color_drop,
+    color_jitter,
+    gaussian_blur,
+    random_apply,
+    scale_image,
+    shuffle_zipped_output,
+    solarize,
+)
+
+
+@pytest.mark.tensorflow
+def test_scale_image() -> None:
+    """Test scale_image."""
+    image = tf.random.uniform((32, 32, 3))
+    label = tf.random.uniform((1,))
+    output = scale_image(image, label)
+    assert output[0].dtype == tf.float32
+    assert output[1].dtype == tf.float32
+    assert output[0].shape == image.shape
+    assert output[1].shape == label.shape
+
+
+@pytest.mark.tensorflow
+@pytest.mark.parametrize("min_val, max_val", [pytest.param(128, 255)])
+def test_solarize(min_val: int, max_val: int) -> None:
+    """Test solarize."""
+    image = tf.random.uniform((32, 32, 3), minval=min_val, maxval=max_val)
+    # convert to float32
+    image = scale_image(image, None)[0]
+    output = solarize(image)
+    assert output.dtype == tf.float32
+    assert output.shape == image.shape
+    # check if the image is solarized
+    assert tf.reduce_all(tf.equal(output, max_val - image))
+
+
+@pytest.mark.tensorflow
+def test_color_drop() -> None:
+    """Test color_drop."""
+    image = tf.random.uniform((32, 32, 3))
+    # convert to float32
+    image = scale_image(image, None)[0]
+    output = color_drop(image)
+    assert output.dtype == tf.float32
+    assert output.shape == image.shape
+    # check if the image is grayscale
+    assert tf.reduce_all(tf.equal(output[:, :, 0], output[:, :, 1]))
+    assert tf.reduce_all(tf.equal(output[:, :, 1], output[:, :, 2]))
+
+
+@pytest.mark.tensorflow
+def test_gaussian_blur() -> None:
+    """Test gaussian_blur."""
+    image = tf.random.uniform((32, 32, 3))
+    # convert to float32
+    image = scale_image(image, None)[0]
+    output = gaussian_blur(image)
+    assert output.dtype == tf.float32
+    assert output.shape == image.shape
+    # check if the image is blurred
+    assert not tf.reduce_all(tf.equal(output, image))
+
+
+@pytest.mark.tensorflow
+def test_color_jitter() -> None:
+    """Test color_jitter."""
+    image = tf.random.uniform((32, 32, 3))
+    # convert to float32
+    image = scale_image(image, None)[0]
+    output = color_jitter(image)
+    assert output.dtype == tf.float32
+    assert output.shape == image.shape
+    # check if the image is jittered
+    assert not tf.reduce_all(tf.equal(output, image))
+
+
+@pytest.mark.tensorflow
+def test_random_apply() -> None:
+    """Test random_apply."""
+    image = tf.random.uniform((32, 32, 3))
+    # convert to float32
+    image = scale_image(image, None)[0]
+    output = random_apply(color_drop, image, prob=1.0)
+    assert output.dtype == tf.float32
+    assert output.shape == image.shape
+    # check if the image is jittered
+    assert not tf.reduce_all(tf.equal(output, image))
+
+
+def test_shuffle_zipped_output() -> None:
+    """Test shuffle_zipped_output."""
+    a, b, c, d, e = (
+        tf.random.uniform((1,)),
+        tf.random.uniform((1,)),
+        tf.random.uniform((1,)),
+        tf.random.uniform((1,)),
+        tf.random.uniform((1,)),
+    )
+
+    output = shuffle_zipped_output(a, b, c, d, e)
+    assert len(output) == 5
