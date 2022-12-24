dca4ce233d9bbac3768696ae90566d8c9d00eae5
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2022-12-24 04:24:54 +0000

    feat(src/PyTorch): add VICReg

---
+++ tests/pytorch/models/test_vicreg.py
@@ -0,0 +1,16 @@
+"""Test VICReg model."""
+import torch
+
+from src.pytorch.models.vicreg import VICReg
+
+BATCH_SIZE = 64
+
+
+def test_vicreg():
+    """Test VICReg model."""
+    model = VICReg(batch_size=BATCH_SIZE)
+    view_1 = torch.rand(BATCH_SIZE, 3, 224, 224)
+    view_2 = torch.rand(BATCH_SIZE, 3, 224, 224)
+    loss = model(view_1, view_2)
+    assert isinstance(loss, torch.Tensor)
+    assert loss.shape == torch.Size([])
